fdbf73e2912c6cf940cd1fd7765579d1665f76fc
336-Daniel <e0850405309@uisrael.edu.ec>
2026-06-13 16:43:06 -0500

    Initial commit

---
+++ svc-prestamos/src/test/java/com/tarea/svc_prestamos/SvcPrestamosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea.svc_prestamos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvcPrestamosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
